dda0496da1d39c2f268a474744fe102f5a4b122c
Wingle Wong <gdmmhym@163.com>
2013-08-13 09:20:34 +0800

    Initial Commit

---
+++ GDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDemo
+//
+//  Created by Wingle Wong on 8/13/13.
+//  Copyright (c) 2013 Wingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ GDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GDemo
+//
+//  Created by Wingle Wong on 8/13/13.
+//  Copyright (c) 2013 Wingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
